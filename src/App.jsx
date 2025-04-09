a1b0ef99790a6cf2ce6cf1c36dd97b40e2e1c334
AnahiVera <vera.anahi.93@gmail.com>
2025-04-09 14:58:47 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  useEffect(() => {
+    console.log('effect')
+    noteService.getAll()
+  .then(initialNotes => {
+    setNotes(initialNotes)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = event => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  /* const updateNote = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note}
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  } */
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+
+export default App
